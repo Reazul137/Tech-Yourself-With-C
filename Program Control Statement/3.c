9b0a091b3f3c639e92fcc8c75878789d19a82bd8
Md Reazul Islam <reazulislam137@gmail.com>
2019-02-13 22:21:26 +0600

    Add files via upload

---
+++ Program Control Statement/3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+
+    printf("Enter a number : ");
+    scanf("%d", &i);
+
+    if((i%2)==0)
+        printf("Even");
+    if((i%2)==1)
+        printf("Odd");
+
+
+    return 0;
+}
